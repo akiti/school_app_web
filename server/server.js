a04d3ca1a1ed5b35a36c93a9752f38232ea12332
akiti <acceptupgrade@gmail.com>
2021-01-21 04:45:17 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'})
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server is listening on port ${port}`))
